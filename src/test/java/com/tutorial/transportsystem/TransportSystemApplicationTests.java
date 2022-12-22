7f6f5f97c1f04f740b31752be83192f2cbfea9b3
Konstantin-git-file <d2d.boxmail@gmail.com>
2022-12-22 20:09:40 +0300

    Initial commit

---
+++ src/test/java/com/tutorial/transportsystem/TransportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.transportsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
